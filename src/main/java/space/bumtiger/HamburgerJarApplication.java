917081bb308eda39dc3f0677c20661ccb82ffa84
osparking <jbpark03@gmail.com>
2023-02-03 07:03:25 +0900

    just created spring boot project

---
+++ src/main/java/space/bumtiger/HamburgerJarApplication.java
@@ -0,0 +1,13 @@
+package space.bumtiger;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HamburgerJarApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HamburgerJarApplication.class, args);
+	}
+
+}
---
+++ src/test/java/space/bumtiger/HamburgerJarApplicationTests.java
@@ -0,0 +1,13 @@
+package space.bumtiger;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HamburgerJarApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
