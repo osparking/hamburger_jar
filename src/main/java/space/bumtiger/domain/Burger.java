7d9105f7c68d0baa0b9a815575ca07cc1253c28e
osparking <jbpark03@gmail.com>
2023-02-11 18:16:08 +0900

    Burger validation rules applied

--- src/main/java/space/bumtiger/domain/Burger.java
+++ src/main/java/space/bumtiger/domain/Burger.java
@@ -2,11 +2,18 @@ package space.bumtiger.domain;
 
 import java.util.List;
 
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
 import lombok.Data;
 
 @Data
 public class Burger {
+	@NotNull
+	@Size(min=2, message="이름은 최소 2 문자만 가능합니다.")
 	private String name;
+
+	@NotNull
+	@Size(min=1, message="고객님, 최소 1개의 재료를 선택하세요.")
 	private List<Ingredient> ingredients;
 	
 }
