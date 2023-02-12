dbfe55b728effa92cc8b163c5e9d9d947c598280
osparking <jbpark03@gmail.com>
2023-02-12 22:48:12 +0900

    id and creation time added

--- src/main/java/space/bumtiger/domain/Burger.java
+++ src/main/java/space/bumtiger/domain/Burger.java
@@ -1,5 +1,6 @@
 package space.bumtiger.domain;
 
+import java.time.LocalDateTime;
 import java.util.List;
 
 import jakarta.validation.constraints.NotNull;
@@ -8,12 +9,17 @@ import lombok.Data;
 
 @Data
 public class Burger {
+
+	private Long id;
+
+	private LocalDateTime createdAt = LocalDateTime.now();
+
 	@NotNull
-	@Size(min=2, message="이름은 최소 2 문자만 가능합니다.")
+	@Size(min = 2, message = "이름은 최소 2 문자만 가능합니다.")
 	private String name;
 
 	@NotNull
-	@Size(min=1, message="고객님, 최소 1개의 재료를 선택하세요.")
+	@Size(min = 1, message = "고객님, 최소 1개의 재료를 선택하세요.")
 	private List<Ingredient> ingredients;
-	
+
 }
