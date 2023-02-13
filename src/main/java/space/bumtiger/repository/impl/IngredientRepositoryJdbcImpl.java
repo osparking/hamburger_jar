21c687fc0282b22875be6561e712648f71f7e05e
osparking <jbpark03@gmail.com>
2023-02-13 12:33:55 +0900

    jdbcTemplate used by 2 methods

--- src/main/java/space/bumtiger/repository/impl/IngredientRepositoryJdbcImpl.java
+++ src/main/java/space/bumtiger/repository/impl/IngredientRepositoryJdbcImpl.java
@@ -1,5 +1,8 @@
 package space.bumtiger.repository.impl;
 
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -10,28 +13,39 @@ import space.bumtiger.domain.Ingredient;
 import space.bumtiger.repository.IngredientRepository;
 
 @Repository
-public class IngredientRepositoryJdbcImpl 
-																implements IngredientRepository {
+public class IngredientRepositoryJdbcImpl implements IngredientRepository {
 
 	private JdbcTemplate jdbcTemplate;
 
-	@Autowired
 	public IngredientRepositoryJdbcImpl(JdbcTemplate jdbcTemplate) {
 		super();
 		this.jdbcTemplate = jdbcTemplate;
 	}
 
+	// @formatter:off
+	private Ingredient mapRowToIngredient(ResultSet row, int rowNum)
+			throws SQLException {
+		return new Ingredient(
+				row.getString("id"), 
+				row.getString("name"),
+				Ingredient.Type.valueOf(row.getString("type")));
+	}
+
 	@Override
 	public Iterable<Ingredient> findAll() {
-		// TODO Auto-generated method stub
-		return null;
+		return jdbcTemplate.query("select id, name, type from Ingredient",
+				this::mapRowToIngredient);
 	}
 
 	@Override
 	public Optional<Ingredient> findById(String id) {
-		// TODO Auto-generated method stub
-		return Optional.empty();
+		List<Ingredient> results = jdbcTemplate.query(
+				"select id, name, type from Ingredient where id=?",
+				this::mapRowToIngredient, id);
+		return results.size() == 0 ? Optional.empty() : 
+																 Optional.of(results.get(0));
 	}
+	// @formatter:on
 
 	@Override
 	public Ingredient save(Ingredient ingredient) {
