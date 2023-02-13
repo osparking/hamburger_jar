4217c37538a1c14a822e181b3cfbae66d17d51c2
osparking <jbpark03@gmail.com>
2023-02-13 13:00:24 +0900

    autowired removed

--- src/main/java/space/bumtiger/repository/impl/IngredientRepositoryJdbcImpl.java
+++ src/main/java/space/bumtiger/repository/impl/IngredientRepositoryJdbcImpl.java
@@ -5,7 +5,6 @@ import java.sql.SQLException;
 import java.util.List;
 import java.util.Optional;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.stereotype.Repository;
 
