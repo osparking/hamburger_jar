4534753fb1715c24c807cac130f0a4d39e57ad4d
osparking <jbpark03@gmail.com>
2023-02-14 14:58:26 +0900

    read ingredient data from DB

--- src/main/java/space/bumtiger/controller/DesignBurgerController.java
+++ src/main/java/space/bumtiger/controller/DesignBurgerController.java
@@ -3,7 +3,9 @@ package space.bumtiger.controller;
 import java.util.Arrays;
 import java.util.List;
 import java.util.stream.Collectors;
+import java.util.stream.StreamSupport;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.Errors;
@@ -19,12 +21,20 @@ import space.bumtiger.domain.Burger;
 import space.bumtiger.domain.BurgerOrder;
 import space.bumtiger.domain.Ingredient;
 import space.bumtiger.domain.Ingredient.Type;
+import space.bumtiger.repository.IngredientRepository;
 
 @Slf4j
 @Controller
 @RequestMapping("/design")
 @SessionAttributes("burgerOrder")
 public class DesignBurgerController {
+
+	private IngredientRepository repository;
+
+	public DesignBurgerController(IngredientRepository repository) {
+		super();
+		this.repository = repository;
+	}
 
 	@PostMapping
 	public String processBurger(@Valid Burger burger, Errors errors,
@@ -38,21 +48,24 @@ public class DesignBurgerController {
 			return "redirect:/orders/current";
 		}
 	}
+	/*
+	Arrays.asList(
+	new Ingredient("BNBD", "번빵", Type.BREAD),
+	new Ingredient("RCBD", "쌀빵", Type.BREAD),
+	new Ingredient("BLGG", "불고기", Type.PROTEIN),
+	new Ingredient("CHBM", "닭가슴살", Type.PROTEIN),
+	new Ingredient("SHMP", "새우", Type.PROTEIN),
+	new Ingredient("TMTO", "토마토", Type.VEGGIES),
+	new Ingredient("LETC", "상추", Type.VEGGIES),
+	new Ingredient("CDCS", "체다치즈", Type.CHEESE),
+	new Ingredient("MLCS", "모짜렐라치즈", Type.CHEESE),
+	new Ingredient("BGSC", "버거소스", Type.SAUCE),
+	new Ingredient("BBSC", "바베큐소스", Type.SAUCE));
+	 */
 
 	@ModelAttribute
 	public void addIngredientsToModel(Model model) {
-		List<Ingredient> ingredients = Arrays.asList(
-				new Ingredient("BNBD", "번빵", Type.BREAD),
-				new Ingredient("RCBD", "쌀빵", Type.BREAD),
-				new Ingredient("BLGG", "불고기", Type.PROTEIN),
-				new Ingredient("CHBM", "닭가슴살", Type.PROTEIN),
-				new Ingredient("SHMP", "새우", Type.PROTEIN),
-				new Ingredient("TMTO", "토마토", Type.VEGGIES),
-				new Ingredient("LETC", "상추", Type.VEGGIES),
-				new Ingredient("CDCS", "체다치즈", Type.CHEESE),
-				new Ingredient("MLCS", "모짜렐라치즈", Type.CHEESE),
-				new Ingredient("BGSC", "버거소스", Type.SAUCE),
-				new Ingredient("BBSC", "바베큐소스", Type.SAUCE));
+		Iterable<Ingredient> ingredients = repository.findAll();
 
 		Type[] types = Ingredient.Type.values();
 		for (Type type : types) {
@@ -76,10 +89,10 @@ public class DesignBurgerController {
 		return "design";
 	}
 
-	private Iterable<Ingredient> filterByType(List<Ingredient> ingredients,
+	private Iterable<Ingredient> filterByType(Iterable<Ingredient> ingredients,
 			Type type) {
-		return ingredients.stream().filter(x -> x.getType().equals(type))
-				.collect(Collectors.toList());
+		return StreamSupport.stream(ingredients.spliterator(), false)
+				.filter(x -> (x.getType().equals(type))).collect(Collectors.toList());
 	}
 
 }
