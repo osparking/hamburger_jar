e53ccfd7ecc091348d29aef2ce471a24c9107c9a
osparking <jbpark03@gmail.com>
2023-02-16 06:51:57 +0900

    initial data for ingredient table

---
+++ src/main/resources/data.sql
@@ -0,0 +1,17 @@
+delete from corder_burger;
+delete from corder;
+delete from burger_ingre;
+delete from burger;
+delete from ingredient;
+
+insert into ingredient values('BNBD', '번빵', 'BREAD');
+insert into ingredient values('RCBD', '쌀빵', 'BREAD');
+insert into ingredient values('BLGG', '불고기', 'PROTEIN');
+insert into ingredient values('CHBM', '닭가슴살', 'PROTEIN');
+insert into ingredient values('SHMP', '새우', 'PROTEIN');
+insert into ingredient values('TMTO', '토마토', 'VEGGIES');
+insert into ingredient values('LETC', '상추', 'VEGGIES');
+insert into ingredient values('CDCS', '체다치즈', 'CHEESE');
+insert into ingredient values('MLCS', '모짜렐라치즈', 'CHEESE');
+insert into ingredient values('BGSC', '버거소스', 'SAUCE');
+insert into ingredient values('BBSC', '바베큐소스', 'SAUCE');
