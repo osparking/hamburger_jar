adfde71791264ddb23c7c1e53ed063c0e48262cd
osparking <jbpark03@gmail.com>
2023-02-18 15:33:00 +0900

    ConfigurationPropertiesScan anno

--- src/main/java/space/bumtiger/HamburgerJarApplication.java
+++ src/main/java/space/bumtiger/HamburgerJarApplication.java
@@ -2,8 +2,12 @@ package space.bumtiger;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.ConfigurationPropertiesScan;
+
+import space.bumtiger.config.CredentialProperties;
 
 @SpringBootApplication
+@ConfigurationPropertiesScan("space.bumtiger.config")
 public class HamburgerJarApplication {
 
 	public static void main(String[] args) {
