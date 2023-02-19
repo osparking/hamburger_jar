6cb06039cce10045381eeb47c1f12954d4295b7e
osparking <jbpark03@gmail.com>
2023-02-19 12:19:05 +0900

    shorter code when CrudRepository used

--- src/main/java/space/bumtiger/repository/IngredientRepository.java
+++ src/main/java/space/bumtiger/repository/IngredientRepository.java
@@ -1,17 +1,10 @@
 package space.bumtiger.repository;
 
-import java.util.Optional;
+import org.springframework.data.repository.CrudRepository;
+
 import space.bumtiger.domain.Ingredient;
 
-import org.springframework.data.repository.Repository;
-
 public interface IngredientRepository 
-				extends Repository<Ingredient, String>{
-
-	Iterable<Ingredient> findAll();
-
-	Optional<Ingredient> findById(String id);
-
-	Ingredient save(Ingredient ingredient);
+				extends CrudRepository<Ingredient, String>{
 
 }
