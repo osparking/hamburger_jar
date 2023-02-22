2a1703a8c47c9224b297dd264364352f6078ac3a
osparking <jbpark03@gmail.com>
2023-02-22 23:03:54 +0900

    used ordinary Integer key type

--- src/main/java/space/bumtiger/repository/CorderRepository.java
+++ src/main/java/space/bumtiger/repository/CorderRepository.java
@@ -5,5 +5,5 @@ import org.springframework.data.repository.CrudRepository;
 import space.bumtiger.domain.Corder;
 
 public interface CorderRepository 
-				extends CrudRepository<Corder, Long> {
+				extends CrudRepository<Corder, Integer> {
 }
