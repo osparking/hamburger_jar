bb5dc7305f83140420df2a2bd6cbf0b1e2d537d8
osparking <jbpark03@gmail.com>
2023-02-22 22:46:16 +0900

    Burger aggregates BurgerIngredients

--- src/main/java/space/bumtiger/domain/Burger.java
+++ src/main/java/space/bumtiger/domain/Burger.java
@@ -15,16 +15,16 @@ import lombok.Data;
 public class Burger {
 
 	@Id
-	private Long id;
-
-	private LocalDateTime createdAt = LocalDateTime.now();
+	private Integer id;
 
 	@NotNull
 	@Size(min = 2, message = "이름은 최소 2 문자만 가능합니다.")
 	private String name;
+	
+	private LocalDateTime createdAt = LocalDateTime.now();
 
 	@NotNull
 	@Size(min = 1, message = "고객님, 최소 1개의 재료를 선택하세요.")
-	private List<Ingredient> ingredients;
+	private List<BurgerIngredient> ingredients;
 
 }
