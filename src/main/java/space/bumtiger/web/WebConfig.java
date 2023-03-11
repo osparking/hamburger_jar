ef7338bd0a0f7e3e8b0681ced63a96ffa973240a
osparking <jbpark03@gmail.com>
2023-03-11 11:57:54 +0900

    minor - just white space change

--- src/main/java/space/bumtiger/web/WebConfig.java
+++ src/main/java/space/bumtiger/web/WebConfig.java
@@ -16,20 +16,20 @@ public class WebConfig implements WebMvcConfigurer {
 	@Bean
 	CommandLineRunner loadIngredient(IngredientRepository repository) {
 		return args -> {
-			repository.save(new Ingredient("BNBD", "번빵", Type.BREAD));     
-			repository.save(new Ingredient("RCBD", "쌀빵", Type.BREAD));     
-			repository.save(new Ingredient("BLGG", "불고기", Type.PROTEIN)); 
+			repository.save(new Ingredient("BNBD", "번빵", Type.BREAD));
+			repository.save(new Ingredient("RCBD", "쌀빵", Type.BREAD));
+			repository.save(new Ingredient("BLGG", "불고기", Type.PROTEIN));
 			repository.save(new Ingredient("CHBM", "닭가슴살", Type.PROTEIN));
-			repository.save(new Ingredient("SHMP", "새우", Type.PROTEIN));   
-			repository.save(new Ingredient("TMTO", "토마토", Type.VEGGIES)); 
-			repository.save(new Ingredient("LETC", "상추", Type.VEGGIES));   
+			repository.save(new Ingredient("SHMP", "새우", Type.PROTEIN));
+			repository.save(new Ingredient("TMTO", "토마토", Type.VEGGIES));
+			repository.save(new Ingredient("LETC", "상추", Type.VEGGIES));
 			repository.save(new Ingredient("CDCS", "체다치즈", Type.CHEESE));
 			repository.save(new Ingredient("MLCS", "모짜렐라치즈", Type.CHEESE));
-			repository.save(new Ingredient("BGSC", "버거소스", Type.SAUCE)); 
+			repository.save(new Ingredient("BGSC", "버거소스", Type.SAUCE));
 			repository.save(new Ingredient("BBSC", "바베큐소스", Type.SAUCE));
 		};
 	}
-	
+
 	@Override
 	public void addViewControllers(ViewControllerRegistry registry) {
 		registry.addViewController("/").setViewName("home");
