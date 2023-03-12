255ee869cf7a77d30e3cf2e3e4b050aca3369332
osparking <jbpark03@gmail.com>
2023-03-12 11:07:21 +0900

    chain for custom security config'

--- src/main/java/space/bumtiger/security/SecurityConfiguration.java
+++ src/main/java/space/bumtiger/security/SecurityConfiguration.java
@@ -2,16 +2,23 @@ package space.bumtiger.security;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
 
 import space.bumtiger.domain.User;
 import space.bumtiger.repository.UserRepository;
 
 @Configuration
 public class SecurityConfiguration {
+	@Bean
+	SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+		return http.build();
+	}
+
 	@Bean
 	PasswordEncoder passwordEncoder() {
 		return new BCryptPasswordEncoder();
