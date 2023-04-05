ebb903e8393de15786c5d44d382e665a3c041b8b
osparking <jbpark03@gmail.com>
2023-04-05 23:15:29 +0900

    new facebook user added to DB

--- src/main/java/space/bumtiger/security/SecurityConfiguration.java
+++ src/main/java/space/bumtiger/security/SecurityConfiguration.java
@@ -1,14 +1,22 @@
 package space.bumtiger.security;
 
+import java.io.IOException;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.core.Authentication;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.AuthenticationSuccessHandler;
 
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
 import space.bumtiger.domain.User;
 import space.bumtiger.repository.UserRepository;
 
@@ -24,11 +32,28 @@ public class SecurityConfiguration {
 			  .formLogin().loginPage("/login")
 				.and()
 				.oauth2Login(oauth2 -> oauth2
-					.loginPage("/login"));
-
+					.loginPage("/login")
+					.userInfoEndpoint()
+					.userService(oauth2UserService)
+				.and()
+					.successHandler(new AuthenticationSuccessHandler() {
+						@Override
+						public void onAuthenticationSuccess(HttpServletRequest request,
+								HttpServletResponse response, Authentication authentication)
+								throws IOException, ServletException {
+							CustomOAuth2User user = (CustomOAuth2User) authentication
+									.getPrincipal();
+							oauth2UserService.processOAuthPostLogin(user.getEmail());
+							response.sendRedirect("/");
+						}
+					})					
+			);
 		return http.build();
 	}
 	// @formatter:on
+
+  @Autowired
+  private CustomOAuth2UserService oauth2UserService;
 
 	@Bean
 	PasswordEncoder passwordEncoder() {
