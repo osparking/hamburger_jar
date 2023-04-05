e19495bf9341db949ca1a99a95dafc7afac9d64c
osparking <jbpark03@gmail.com>
2023-04-06 06:35:43 +0900

    ingredients view controller added

---
+++ src/main/java/space/bumtiger/config/MvcConfig.java
@@ -0,0 +1,12 @@
+package space.bumtiger.config;
+
+import org.springframework.context.annotation.*;
+import org.springframework.web.servlet.config.annotation.*;
+
+@Configuration
+public class MvcConfig implements WebMvcConfigurer {
+	@Override
+	public void addViewControllers(ViewControllerRegistry registry) {
+		registry.addViewController("/ingredient/list").setViewName("ingredients");
+	}
+}
