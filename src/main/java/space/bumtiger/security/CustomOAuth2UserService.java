04770a91c42e7a2bbbb8c2ba8053d6ac68e9b1d6
osparking <jbpark03@gmail.com>
2023-04-05 23:04:26 +0900

    Service class for facebook user

---
+++ src/main/java/space/bumtiger/security/CustomOAuth2UserService.java
@@ -0,0 +1,35 @@
+package space.bumtiger.security;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.oauth2.client.userinfo.DefaultOAuth2UserService;
+import org.springframework.security.oauth2.client.userinfo.OAuth2UserRequest;
+import org.springframework.security.oauth2.core.OAuth2AuthenticationException;
+import org.springframework.security.oauth2.core.user.OAuth2User;
+import org.springframework.stereotype.Service;
+
+import space.bumtiger.domain.User;
+import space.bumtiger.repository.UserRepository;
+
+@Service
+public class CustomOAuth2UserService extends DefaultOAuth2UserService {
+	@Override
+	public OAuth2User loadUser(OAuth2UserRequest userRequest)
+			throws OAuth2AuthenticationException {
+		OAuth2User user = super.loadUser(userRequest);
+		return new CustomOAuth2User(user);
+	}
+
+	@Autowired
+	private UserRepository userRepo;
+
+	public void processOAuthPostLogin(String email) {
+		User dbUser = userRepo.findByUsername(email);
+
+		if (dbUser == null) {
+			User facebookUser = new User(email, "(NA)", "ROLE_USER", true,
+					Provider.FACEBOOK);
+			userRepo.save(facebookUser);
+		}
+	}
+
+}
