7050f8ec418207ea4827ce3c352e84ecfef09188
osparking <jbpark03@gmail.com>
2023-04-05 20:03:27 +0900

    ROLE_USER set to existing users

--- src/main/resources/sql/alter_user.sql
+++ src/main/resources/sql/alter_user.sql
@@ -1,3 +1,4 @@
 ALTER TABLE hamburger.`user` ADD `role` varchar(45) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NOT NULL;
 ALTER TABLE hamburger.`user` ADD enabled tinyint(1) DEFAULT 0 NULL;
 ALTER TABLE hamburger.`user` ADD provider varchar(15) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci DEFAULT NULL NULL;
+ALTER TABLE hamburger.`user` MODIFY COLUMN `role` varchar(45) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci DEFAULT 'ROLE_USER' NOT NULL;
