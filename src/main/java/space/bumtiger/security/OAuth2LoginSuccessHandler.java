2617808d089722cf8d865de59aaa1e61baea04be
osparking <jbpark03@gmail.com>
2023-04-08 21:18:30 +0900

    class that redirects to original page

---
+++ src/main/java/space/bumtiger/security/OAuth2LoginSuccessHandler.java
@@ -0,0 +1,31 @@
+package space.bumtiger.security;
+
+import java.io.IOException;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.web.authentication.SavedRequestAwareAuthenticationSuccessHandler;
+import org.springframework.stereotype.Component;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import space.bumtiger.domain.User;
+import space.bumtiger.repository.UserRepository;
+
+@Component
+public class OAuth2LoginSuccessHandler
+		extends SavedRequestAwareAuthenticationSuccessHandler {
+	@Autowired
+	private CustomOAuth2UserService userService;
+
+	@Override
+	public void onAuthenticationSuccess(HttpServletRequest request,
+			HttpServletResponse response, Authentication authentication)
+			throws IOException, ServletException {
+
+		CustomOAuth2User user = (CustomOAuth2User) authentication.getPrincipal();
+		userService.processOAuthPostLogin(user.getEmail());
+		super.onAuthenticationSuccess(request, response, authentication);
+	}
+}
