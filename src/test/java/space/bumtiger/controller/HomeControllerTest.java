2caca96fa50b16724a957b8ba1bab9535ceb8b84
osparking <jbpark03@gmail.com>
2023-04-10 11:42:29 +0900

    junit test no bean error fix

--- src/test/java/space/bumtiger/controller/HomeControllerTest.java
+++ src/test/java/space/bumtiger/controller/HomeControllerTest.java
@@ -1,22 +1,27 @@
 package space.bumtiger.controller;
 
 import static org.hamcrest.CoreMatchers.containsString;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
-
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.view;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.test.web.servlet.MockMvc;
+
+import space.bumtiger.repository.IngredientRepository;
 
 @WebMvcTest
 class HomeControllerTest {
 	@Autowired
 	private MockMvc mockMvc;
 
+	@MockBean
+	IngredientRepository ingredientRepository;
+	
 	@Test
 	void test() throws Exception {
 		mockMvc.perform(get("/")).andExpect(status().isOk())
