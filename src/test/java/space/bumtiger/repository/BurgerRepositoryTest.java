39554bc9538460a2c323b5948d8a2dec0476d5d8
osparking <jbpark03@gmail.com>
2023-04-20 14:17:19 +0900

    tests 4 burger repository methods - v.1

---
+++ src/test/java/space/bumtiger/repository/BurgerRepositoryTest.java
@@ -0,0 +1,29 @@
+package space.bumtiger.repository;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class BurgerRepositoryTest {
+
+	@Test
+	void testSave() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testFindById() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testFindAll() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testDeleteById() {
+		fail("Not yet implemented");
+	}
+
+}
