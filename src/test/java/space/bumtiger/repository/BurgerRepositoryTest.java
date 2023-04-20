2734131b7e5910575cfbed1ea8ffd3c79a998523
osparking <jbpark03@gmail.com>
2023-04-20 21:23:46 +0900

    test if hamburger saves well

--- src/test/java/space/bumtiger/repository/BurgerRepositoryTest.java
+++ src/test/java/space/bumtiger/repository/BurgerRepositoryTest.java
@@ -1,14 +1,36 @@
 package space.bumtiger.repository;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.fail;
 
+import java.time.LocalDateTime;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.data.jdbc.DataJdbcTest;
 
+import space.bumtiger.domain.Burger;
+
+@DataJdbcTest
 class BurgerRepositoryTest {
 
+	@Autowired
+	private BurgerRepository repository;
+	private Burger cheeseBurger;
+	
 	@Test
+	@DisplayName("햄버거 - 저장소 - 저장 성공!")
 	void testSave() {
-		fail("Not yet implemented");
+		// Arrange
+		// Act
+		var savedBurger = repository.save(cheeseBurger);
+
+		// Assert
+		assertNotNull(savedBurger);
+		assertThat(savedBurger.getId()).isNotEqualTo(null);
 	}
 
 	@Test
@@ -26,4 +48,10 @@ class BurgerRepositoryTest {
 		fail("Not yet implemented");
 	}
 
+	@BeforeEach
+	void cookBurgers() {
+		cheeseBurger = new Burger();
+		cheeseBurger.setCreatedAt(LocalDateTime.now());
+		cheeseBurger.setName("범스치킨버거");
+	}
 }
