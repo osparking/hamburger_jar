f5a125c3e545da0d9ab2dc37d55bafc1af26202d
osparking <jbpark03@gmail.com>
2023-04-23 06:51:10 +0900

    test corder-burger repo CRUD

---
+++ src/test/java/space/bumtiger/repository/CorderBurgerRepositoryTest.java
@@ -0,0 +1,39 @@
+package space.bumtiger.repository;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+class CorderBurgerRepositoryTest {
+
+	@BeforeEach
+	void setUp() throws Exception {
+	}
+
+	@AfterEach
+	void tearDown() throws Exception {
+	}
+
+	@Test
+	void testSave() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testSaveAll() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testFindAllById() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testDeleteAllById() {
+		fail("Not yet implemented");
+	}
+
+}
