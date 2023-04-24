aa99d0616214742b3200c81d407c8ea75b5d0d4d
osparking <jbpark03@gmail.com>
2023-04-24 13:24:02 +0900

    burger controller tester stub

---
+++ src/test/java/space/bumtiger/controller/BurgerControllerTest.java
@@ -0,0 +1,29 @@
+package space.bumtiger.controller;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+class BurgerControllerTest {
+
+	@BeforeEach
+	void setUp() throws Exception {
+	}
+
+	@AfterEach
+	void tearDown() throws Exception {
+	}
+
+	@Test
+	void testProcessBurger() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testShowDesignForm() {
+		fail("Not yet implemented");
+	}
+
+}
