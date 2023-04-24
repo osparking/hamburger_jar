de809f79699e2da703275ec67dd9f95b45d1f5c5
osparking <jbpark03@gmail.com>
2023-04-24 09:13:19 +0900

    custom method of corderburgerrepo inter'

--- src/main/java/space/bumtiger/repository/CorderBurgerRepository.java
+++ src/main/java/space/bumtiger/repository/CorderBurgerRepository.java
@@ -4,7 +4,12 @@ import org.springframework.data.repository.CrudRepository;
 
 import space.bumtiger.domain.CorderBurger;
 
-public interface CorderBurgerRepository 
-				extends CrudRepository<CorderBurger, Integer> {
+public interface CorderBurgerRepository
+		extends CrudRepository<CorderBurger, Integer> {
 	Iterable<CorderBurger> findAllByCorder(int order);
+
+	default void deleteByCorder(int corder) {
+		var items = this.findAllByCorder(corder);
+		items.iterator().forEachRemaining(row -> this.delete(row));
+	}
 }
