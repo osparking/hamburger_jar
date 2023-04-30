baf9f5166bd621478dce76bcd208f6955c544349
osparking <jbpark03@gmail.com>
2023-04-30 17:49:23 +0900

    service for corder class

---
+++ src/main/java/space/bumtiger/service/CorderService.java
@@ -0,0 +1,16 @@
+package space.bumtiger.service;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import space.bumtiger.repository.CorderRepository;
+
+@Service
+public class CorderService {
+	@Autowired
+	private CorderRepository repository;
+	
+	public void deleteAllOrders() {
+		repository.deleteAll();
+	}
+}
