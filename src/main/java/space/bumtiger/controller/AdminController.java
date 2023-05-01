f012b1b3add13eb91cb24e38ccd69760a8b270fc
osparking <jbpark03@gmail.com>
2023-05-01 22:26:03 +0900

    admin URL request handler

--- src/main/java/space/bumtiger/controller/AdminController.java
+++ src/main/java/space/bumtiger/controller/AdminController.java
@@ -1,6 +1,9 @@
 package space.bumtiger.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
@@ -9,7 +12,14 @@ import space.bumtiger.service.CorderAdminService;
 @Controller
 @RequestMapping("/admin")
 public class AdminController {
+	@Autowired
 	private CorderAdminService service;
+
+	@GetMapping
+	public String adminPage(Model model) {
+		model.addAttribute("orderCount", service.getOrderCount());
+		return "admin";
+	}
 
 	@PostMapping("/deleteOrders")
 	public String deleteAllOrders() {
