0e686cea812c4b43383109f33d82b4ac5f02dbc8
osparking <jbpark03@gmail.com>
2023-05-06 07:13:10 +0900

    saves userId for a burger order

--- src/main/java/space/bumtiger/controller/CorderController.java
+++ src/main/java/space/bumtiger/controller/CorderController.java
@@ -1,6 +1,6 @@
 package space.bumtiger.controller;
 
-import java.net.http.HttpRequest;
+import java.security.Principal;
 import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
 
@@ -17,8 +17,10 @@ import org.springframework.web.bind.support.SessionStatus;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.validation.Valid;
 import space.bumtiger.domain.Corder;
+import space.bumtiger.domain.User;
 import space.bumtiger.repository.CorderBurgerRepository;
 import space.bumtiger.repository.CorderRepository;
+import space.bumtiger.repository.UserRepository;
 
 @Controller
 @RequestMapping("/orders")
@@ -26,12 +28,15 @@ import space.bumtiger.repository.CorderRepository;
 public class CorderController {
 	private CorderRepository repository;
 	private CorderBurgerRepository cbRepository;
+	private UserRepository userRepository;
 
 	public CorderController(CorderRepository corderRepository,
-			CorderBurgerRepository corderBurgerRepository) {
+			CorderBurgerRepository corderBurgerRepository, 
+			UserRepository userRepository) {
 		super();
 		this.repository = corderRepository;
 		this.cbRepository = corderBurgerRepository;
+		this.userRepository = userRepository;
 	}
 
 	@GetMapping("current")
@@ -65,13 +70,16 @@ public class CorderController {
 
 	@PostMapping
 	public String processOrder(@Valid Corder corder, Errors errors,
-			SessionStatus sessionStatus) {
+			SessionStatus sessionStatus, Principal principal) {
 		if (errors.hasErrors()) {
 			return "orderForm";
 		}
 		/** 
 		 * 주문 자체 저장 
 		 */
+		User user = userRepository.findByUsername(principal.getName());
+
+		corder.setUserId(user.getId());
 		corder.setPlacedAt(LocalDateTime.now());
 		Corder corderSaved = repository.save(corder);
 		
