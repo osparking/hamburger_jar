7729997f58f79f16b9f5b29aac23a76b19f3ffac
osparking <jbpark03@gmail.com>
2023-05-08 09:44:12 +0900

    idLocal is for CustomOAuth2User type principal

--- src/main/java/space/bumtiger/security/CustomOAuth2User.java
+++ src/main/java/space/bumtiger/security/CustomOAuth2User.java
@@ -3,9 +3,11 @@ package space.bumtiger.security;
 import java.io.Serializable;
 import java.util.ArrayList;
 import java.util.Collection;
+import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
+import org.springframework.data.annotation.Transient;
 import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.oauth2.core.user.OAuth2User;
@@ -16,10 +18,13 @@ import lombok.AllArgsConstructor;
 public class CustomOAuth2User implements OAuth2User, Serializable {
 	private static final long serialVersionUID = 1L;
 	private OAuth2User oAuth2User;
+	
+	@Transient
+	private Map<String, Object> attributes;
 
 	@Override
 	public Map<String, Object> getAttributes() {
-		return oAuth2User.getAttributes();
+		return attributes;
 	}
 
 	@Override
@@ -38,5 +43,16 @@ public class CustomOAuth2User implements OAuth2User, Serializable {
 
 	public String getEmail() {
 		return oAuth2User.<String>getAttribute("email");
+	}
+	
+	public Integer getIdLocal() {
+		return (Integer)attributes.get("idLocal");
+	}
+
+	public CustomOAuth2User(OAuth2User oAuth2User) {
+		super();
+		this.oAuth2User = oAuth2User;
+		this.attributes = new HashMap<String, Object>(); 
+		attributes.putAll(oAuth2User.getAttributes());
 	}
 }
