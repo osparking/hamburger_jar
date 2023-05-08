57ee1fdefa6107105edf7a6e7109d2412fafa067
osparking <jbpark03@gmail.com>
2023-05-08 10:45:30 +0900

    set and use readable member of corder object

--- src/main/java/space/bumtiger/service/CorderService.java
+++ src/main/java/space/bumtiger/service/CorderService.java
@@ -1,30 +1,34 @@
 package space.bumtiger.service;
 
-import java.security.Principal;
-
 import org.springframework.security.access.prepost.PostAuthorize;
+import org.springframework.security.core.Authentication;
 import org.springframework.stereotype.Service;
 
 import lombok.AllArgsConstructor;
 import space.bumtiger.domain.Corder;
 import space.bumtiger.domain.User;
 import space.bumtiger.repository.CorderRepository;
-import space.bumtiger.repository.UserRepository;
+import space.bumtiger.security.CustomOAuth2User;
 
 @Service
 @AllArgsConstructor
 public class CorderService {
 
 	private CorderRepository repository;
-	private UserRepository userRepository;
 	
-	@PostAuthorize("hasRole('ADMIN') || " +
-	    "authentication.name == returnObject.username")
-	public Corder getOrder(Integer id, Principal principal) {
+	@PostAuthorize("hasRole('ADMIN') || returnObject.readable")
+	public Corder getOrder(Integer id, Authentication auth) {
 		Corder corder = repository.findById(id).orElseThrow();
-		User user = userRepository.findById(corder.getUserId()).get();
-
-		corder.setUsername(user.getUsername());
+		corder.setReadable(false);
+		var prin = auth.getPrincipal();
+		if (prin instanceof CustomOAuth2User) {
+			var idLocal = ((CustomOAuth2User) prin).getIdLocal();
+			if (corder.getUserId() == idLocal)
+				corder.setReadable(true);
+		} else {
+			if (corder.getUserId() == ((User)prin).getId())
+				corder.setReadable(true);
+		}
 		return corder;
 	}
 }
