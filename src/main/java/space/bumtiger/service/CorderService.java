007ae87155446e5536a4b34a4cb36ae0f40a3e75
osparking <jbpark03@gmail.com>
2023-05-08 21:20:14 +0900

    readable no order exception message

--- src/main/java/space/bumtiger/service/CorderService.java
+++ src/main/java/space/bumtiger/service/CorderService.java
@@ -1,5 +1,7 @@
 package space.bumtiger.service;
 
+import java.util.NoSuchElementException;
+
 import org.springframework.security.access.prepost.PostAuthorize;
 import org.springframework.stereotype.Service;
 
@@ -16,7 +18,14 @@ public class CorderService {
 	
 	@PostAuthorize("hasRole('ADMIN') || returnObject.readable")
 	public Corder getOrder(Integer id, Object user) throws Exception {
-		Corder corder = repository.findById(id).orElseThrow();
+		Corder corder = repository.findById(id).orElseThrow(() -> {
+			var msgBuf = new StringBuffer("ID가 '");
+
+			msgBuf.append(id);
+			msgBuf.append("'인 주문 없습니다.");
+
+			return new NoSuchElementException(msgBuf.toString());
+		});
 		corder.setReadable(false);
 		if (LoginUser.getUserId(user) == corder.getUserId()) {
 			corder.setReadable(true);
