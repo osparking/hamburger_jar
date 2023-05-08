c7e184c96d4ade9c170dfb61335d86be5a1b8d36
osparking <jbpark03@gmail.com>
2023-05-08 18:46:39 +0900

    user-type parameter for authentication

--- src/main/java/space/bumtiger/security/LoginUser.java
+++ src/main/java/space/bumtiger/security/LoginUser.java
@@ -1,17 +1,14 @@
 package space.bumtiger.security;
 
-import org.springframework.security.core.Authentication;
-
 import space.bumtiger.domain.User;
 
 public class LoginUser {
-	public static Integer getUserId(Authentication authentication) {
-		var principal = authentication.getPrincipal();
+	public static Integer getUserId(Object user) {
 		Integer id = null;
-		if (principal instanceof CustomOAuth2User) {
-			id = ((CustomOAuth2User) principal).getIdLocal();
+		if (user instanceof CustomOAuth2User) {
+			id = ((CustomOAuth2User) user).getIdLocal();
 		} else {
-			id = ((User)principal).getId();
+			id = ((User)user).getId();
 		}
 		return id;
 	}
