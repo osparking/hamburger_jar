983a87785f29d0afb6efb961de6968068366302f
osparking <jbpark03@gmail.com>
2023-05-08 18:54:53 +0900

    user-type param instead of authentication

--- src/main/java/space/bumtiger/service/CorderService.java
+++ src/main/java/space/bumtiger/service/CorderService.java
@@ -1,14 +1,11 @@
 package space.bumtiger.service;
 
 import org.springframework.security.access.prepost.PostAuthorize;
-import org.springframework.security.core.Authentication;
 import org.springframework.stereotype.Service;
 
 import lombok.AllArgsConstructor;
 import space.bumtiger.domain.Corder;
-import space.bumtiger.domain.User;
 import space.bumtiger.repository.CorderRepository;
-import space.bumtiger.security.CustomOAuth2User;
 import space.bumtiger.security.LoginUser;
 
 @Service
@@ -18,10 +15,10 @@ public class CorderService {
 	private CorderRepository repository;
 	
 	@PostAuthorize("hasRole('ADMIN') || returnObject.readable")
-	public Corder getOrder(Integer id, Authentication authentication) {
+	public Corder getOrder(Integer id, Object user) throws Exception {
 		Corder corder = repository.findById(id).orElseThrow();
 		corder.setReadable(false);
-		if (LoginUser.getUserId(authentication) == corder.getUserId()) {
+		if (LoginUser.getUserId(user) == corder.getUserId()) {
 			corder.setReadable(true);
 		}
 		return corder;
