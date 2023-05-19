33b63de77d8330f72e8b80acdc330a5266e023fa
osparking <jbpark03@gmail.com>
2023-05-19 21:40:22 +0900

    order list for a given user ID

--- src/main/java/space/bumtiger/repository/CorderRepository.java
+++ src/main/java/space/bumtiger/repository/CorderRepository.java
@@ -1,9 +1,13 @@
 package space.bumtiger.repository;
 
+import java.util.List;
+
 import org.springframework.data.repository.CrudRepository;
 
 import space.bumtiger.domain.Corder;
 
 public interface CorderRepository 
 				extends CrudRepository<Corder, Integer> {
+
+	List<Corder> findByUserIdOrderByPlacedAtDesc(Integer id);
 }
