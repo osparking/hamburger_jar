0e7a9e4dcfed33ec044f5d7f89eb51bf46ca4576
osparking <jbpark03@gmail.com>
2023-05-20 12:35:47 +0900

    supports order list fetch by page

--- src/main/java/space/bumtiger/repository/CorderRepository.java
+++ src/main/java/space/bumtiger/repository/CorderRepository.java
@@ -2,12 +2,16 @@ package space.bumtiger.repository;
 
 import java.util.List;
 
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.repository.CrudRepository;
 
 import space.bumtiger.domain.Corder;
 
+// @formatter:off
 public interface CorderRepository 
-				extends CrudRepository<Corder, Integer> {
+		extends CrudRepository<Corder, Integer> {
 
-	List<Corder> findByUserIdOrderByPlacedAtDesc(Integer id);
+	List<Corder> findByUserIdOrderByPlacedAtDesc(
+			Integer id, Pageable pageable);
 }
+// @formatter:on
