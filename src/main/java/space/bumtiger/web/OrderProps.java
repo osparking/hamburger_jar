f57cb945492a0d22434610c22c589d01c82af8b5
osparking <jbpark03@gmail.com>
2023-05-21 12:48:11 +0900

    pagesize value range is specified

--- src/main/java/space/bumtiger/web/OrderProps.java
+++ src/main/java/space/bumtiger/web/OrderProps.java
@@ -2,12 +2,19 @@ package space.bumtiger.web;
 
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.stereotype.Component;
+import org.springframework.validation.annotation.Validated;
 
+import jakarta.validation.constraints.Max;
+import jakarta.validation.constraints.Min;
 import lombok.Data;
 
 @Component
 @ConfigurationProperties(prefix = "burger.orders")
 @Data
+@Validated
 public class OrderProps {
-	private int pageSize = 3;
+	
+	@Min(value=5, message="5 이상 25 이하 값만 허용됨.")
+	@Max(value=25, message="5 이상 25 이하 값만 허용됨.")
+	private int pageSize = 10;
 }
