bf3103cf83ceccfd2dcc589f255e0dc798df7342
osparking <jbpark03@gmail.com>
2023-05-23 21:44:39 +0900

    insert ingredient rows

---
+++ src/main/resources/data_h2.sql
@@ -0,0 +1,12 @@
+insert into ingredient values
+	('BNBD', '번빵', 'BREAD'),
+	('RCBD', '쌀빵', 'BREAD')	,
+  ('BLGG', '불고기', 'PROTEIN'),   	
+  ('CHBM', '닭가슴살', 'PROTEIN'),  	
+  ('SHMP', '새우', 'PROTEIN'),    	
+  ('TMTO', '토마토', 'VEGGIES'),   	
+  ('LETC', '상추', 'VEGGIES'),    	
+  ('CDCS', '체다치즈', 'CHEESE'),   
+  ('MLCS', '모짜렐라치즈', 'CHEESE'), 
+  ('BGSC', '버거소스', 'SAUCE'),    
+  ('BBSC', '바베큐소스', 'SAUCE');   
