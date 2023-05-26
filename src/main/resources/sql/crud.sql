67fbe73df33a76516b3e134db7f2dd3c803fe4c9
osparking <jbpark03@gmail.com>
2023-05-26 15:45:05 +0900

    burgers that have been ordered

--- src/main/resources/sql/crud.sql
+++ src/main/resources/sql/crud.sql
@@ -1,1 +1,9 @@
 insert into ingredient values('BNBD', '번빵', 'BREAD');
+
+# 주문되었던 모든 버거를 추출한다
+select * 
+from burger b
+where b.id in ( 
+	select cb.burger  
+	from corder_burger cb
+	join corder c on cb.corder = c.id);
