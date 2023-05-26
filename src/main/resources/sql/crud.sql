fa11f998bf126558e80752a8bb6dec584fc29b88
osparking <jbpark03@gmail.com>
2023-05-26 15:48:05 +0900

    update user column of burger table

--- src/main/resources/sql/crud.sql
+++ src/main/resources/sql/crud.sql
@@ -7,3 +7,14 @@ where b.id in (
 	select cb.burger  
 	from corder_burger cb
 	join corder c on cb.corder = c.id);
+	
+# 버거의 user 열에 설게한 사용자 ID 저장
+update burger b set b.user = (
+	select c.user_id
+	from corder_burger cb
+		join corder c on c.id = cb.corder 
+			and cb.burger = b.id)
+where b.id in ( 
+	select cb.burger  
+	from corder_burger cb
+	join corder c on cb.corder = c.id);	
