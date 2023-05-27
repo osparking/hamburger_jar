4c00397307fa7cfbac7a66a1302a17958b6fac58
osparking <jbpark03@gmail.com>
2023-05-27 10:20:04 +0900

    find burgers designed by a user ID

--- src/main/java/space/bumtiger/repository/BurgerRepository.java
+++ src/main/java/space/bumtiger/repository/BurgerRepository.java
@@ -1,9 +1,15 @@
 package space.bumtiger.repository;
 
+import java.util.List;
+
+import org.springframework.data.domain.PageRequest;
 import org.springframework.data.repository.CrudRepository;
 
 import space.bumtiger.domain.Burger;
 
 public interface BurgerRepository 
 				extends CrudRepository<Burger, Integer> {
+
+	List<Burger> findByUserIdOrderByCreatedAtDesc(Integer user,
+			PageRequest page);
 }
