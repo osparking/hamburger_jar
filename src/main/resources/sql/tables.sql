3d7f962b42351c1c94b28cb27f78af48c2a040f7
osparking <jbpark03@gmail.com>
2023-06-11 09:31:10 +0900

    foreign key change allowing ingredient drop

--- src/main/resources/sql/tables.sql
+++ src/main/resources/sql/tables.sql
@@ -3,3 +3,9 @@ create table if not exists Ingredient (
   name varchar(25) not null,
   type varchar(10) not null
 );
+
+ALTER TABLE hamburger.burger_ingredient DROP FOREIGN KEY ingredient_ID_FK2;
+ALTER TABLE hamburger.burger_ingredient ADD CONSTRAINT ingredient_ID_FK2 FOREIGN KEY (ingredient) REFERENCES hamburger.ingredient(id) ON DELETE CASCADE ON UPDATE CASCADE;
+
+ALTER TABLE hamburger.corder_burger DROP FOREIGN KEY corder_burger_FK;
+ALTER TABLE hamburger.corder_burger ADD CONSTRAINT corder_burger_FK FOREIGN KEY (corder) REFERENCES hamburger.corder(id);
