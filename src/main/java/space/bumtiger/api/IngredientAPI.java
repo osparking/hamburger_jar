d7ce700f13266c50b7c158312f7f28770d360d62
osparking <jbpark03@gmail.com>
2023-06-12 12:47:15 +0900

    3 APIs before REST security imple'

---
+++ src/main/java/space/bumtiger/api/IngredientAPI.java
@@ -0,0 +1,41 @@
+package space.bumtiger.api;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestController;
+
+import lombok.AllArgsConstructor;
+import space.bumtiger.domain.Ingredient;
+import space.bumtiger.repository.IngredientRepository;
+
+@RestController
+@RequestMapping(path = "/api/ingredients", produces = "application/json")
+@CrossOrigin(origins = "http://localhost:8080")
+@AllArgsConstructor
+public class IngredientAPI {
+	private IngredientRepository repo;
+
+	@GetMapping
+	public Iterable<Ingredient> allIngredients() {
+		return repo.findAll();
+	}
+
+	@PostMapping
+	@ResponseStatus(HttpStatus.CREATED)
+	public Ingredient saveIngredient(@RequestBody Ingredient ingredient) {
+		return repo.save(ingredient);
+	}
+
+	@DeleteMapping("/{id}")
+	@ResponseStatus(HttpStatus.NO_CONTENT)
+	public void deleteIngredient(@PathVariable("id") String ingredientId) {
+		repo.deleteById(ingredientId);
+	}
+}
