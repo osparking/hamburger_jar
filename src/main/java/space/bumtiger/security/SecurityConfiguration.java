4d676db617ed3900265936ba3463383a2eb151a0
osparking <jbpark03@gmail.com>
2023-06-14 15:12:03 +0900

    secure post&delete request to REST API

--- src/main/java/space/bumtiger/security/SecurityConfiguration.java
+++ src/main/java/space/bumtiger/security/SecurityConfiguration.java
@@ -3,6 +3,7 @@ package space.bumtiger.security;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.core.userdetails.UserDetailsService;
@@ -22,6 +23,8 @@ public class SecurityConfiguration {
 	SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
 		http.authorizeHttpRequests(
 					authorize -> authorize
+						.requestMatchers(HttpMethod.POST, "/api/ingredients").hasRole("ADMIN")
+						.requestMatchers(HttpMethod.DELETE, "/api/ingredients/**").hasRole("ADMIN")
 						.requestMatchers("/design", "/orders").hasRole("USER")
 						.requestMatchers("/ingredient/**").hasRole("ADMIN")
 						.requestMatchers("/", "/**").permitAll())
